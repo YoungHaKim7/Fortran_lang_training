086c61d3b5f58e6b6b5b985d3ef8431c0e83f32e
GYoung <ytok1108@aol.com>
2025-09-07 19:34:18 +0900

    Variables _ Fortran

---
+++ 0001_Fortran_tutorials/a02_variable/src/variables.f90
@@ -0,0 +1,10 @@
+program variables
+  implicit none
+
+  integer :: amount
+  real :: pi, e ! two `real` variables declared
+  complex :: frequency
+  character :: initial
+  logical :: isOkay
+
+end program variables
